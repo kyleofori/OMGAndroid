2ffc3fdd97123411924ae4daba902c252f90f96a
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-01 14:50:04 -0400

    Adding button to activity_main

--- app/src/main/java/com/detroitlabs/kyleofori/omgandroid/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/omgandroid/MainActivity.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1. Access the TextView defined in layout XML and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
     }
 
 

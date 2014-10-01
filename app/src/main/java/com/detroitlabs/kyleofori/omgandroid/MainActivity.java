45a2e6367484397fbcc86ddd636facb3842e137f
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-01 16:51:32 -0400

    Completing Detecting List Selections

--- app/src/main/java/com/detroitlabs/kyleofori/omgandroid/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/omgandroid/MainActivity.java
@@ -2,9 +2,11 @@ package com.detroitlabs.kyleofori.omgandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +16,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener,
+        AdapterView.OnItemClickListener {
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
@@ -41,6 +44,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         //Set the ListView to use the ArrayAdapter
         mainListView.setAdapter(mArrayAdapter);
+
+        // 5. Set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
     }
 
 
@@ -73,5 +79,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mNameList.add(mainEditText.getText().toString());
         mArrayAdapter.notifyDataSetChanged();
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView parent, View view, int position, long id) {
+        // Log the item's position and contents
+        // to the console in Debug
+        Log.d("omg android", position + ": " + mNameList.get(position));
     }
 }
